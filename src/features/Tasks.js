7fdcd8ffe2efb78b1927e66eaf2e51134c7dcece
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2022-12-11 00:23:14 +0100

    removed usetask

--- src/features/Tasks.js
+++ src/features/Tasks.js
@@ -3,34 +3,22 @@ import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import Section from "../common/Section";
 import { StyledContainer } from "../styled.js";
-import { useTasks } from "../useTasks.js";
-import {useDispatch} from 'react-redux'
-// import { useSelector } from "react-redux";
-// import { selectTasks } from "./tasksSlice";
+import { useDispatch } from "react-redux";
 
 function Tasks() {
-  // const { tasks } = useSelector(selectTasks);
 
-  const { toggleTaskDone, removeTask, addNewTask, setAllDone } = useTasks();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  
   return (
     <StyledContainer>
       <main className="container">
         <h1>Lista zadań</h1>
-        <Section
-          title="Dodaj zadanie"
-          body={<Form addNewTask={addNewTask} />}
-        />
+        <Section title="Dodaj zadanie" body={<Form />} />
 
         <Section
           title={"Zadania do zrobienia"}
-          body={
-            <TasksList
-              removeTask={removeTask}
-              toggleTaskDone={toggleTaskDone}
-            />
-          }
-          extraHeaderContent={<Buttons setAllDone={setAllDone} />}
+          body={<TasksList />}
+          extraHeaderContent={<Buttons />}
         />
       </main>
     </StyledContainer>
