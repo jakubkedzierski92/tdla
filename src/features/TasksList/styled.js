5852ab2b40d94d965e436a0d925e4dceeee70467
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2022-12-11 21:13:08 +0100

    formatted code

--- src/features/TasksList/styled.js
+++ src/features/TasksList/styled.js
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 export const List = styled.ul`
   margin: auto;
   padding: 10px;
+
   @media (max-width: 767px) {
     padding: 10px;
     margin: 5px;
@@ -18,6 +19,7 @@ export const ListItem = styled.li`
   border-bottom: 2px solid hsl(0, 0%, 90%);
   column-gap: 10px;
   word-break: break-all;
+
   @media (max-width: 767px) {
     max-width: 255px;
     width: 90%;
@@ -25,17 +27,19 @@ export const ListItem = styled.li`
     align-items: flex-start;
     word-break: break-all;
   }
-  ${({ hide }) => hide && css`
-  display: none;
-  `}
-`
-export const Content = styled.span`
-${({ done }) => done && css`
-  text-decoration: line-through;
-  `}
-  `
+  ${({ hide }) =>
+    hide &&
+    css`
+      display: none;
+    `}
+`;
+  export const Content = styled.span`
+    ${({ done }) => done && css`
+      text-decoration: line-through;
+    `}
+`;
 
-  export const Button = styled.button`
+export const Button = styled.button`
   border: 1px solid rgb(0, 0, 0);
   background-color: teal;
   color: white;
@@ -43,11 +47,16 @@ ${({ done }) => done && css`
   height: 25px;
   padding: 0;
   cursor: grab;
-  ${({ done }) => done && css`
-  background-color: teal;
-  `}
-  ${({ remove }) => remove && css`
-  border: none;
-  background-color: transparent;
-  `}
-  `
+  
+  ${({ done }) =>
+    done &&
+    css`
+      background-color: teal;
+    `}
+  ${({ remove }) =>
+    remove &&
+    css`
+      border: none;
+      background-color: transparent;
+    `}
+`;
